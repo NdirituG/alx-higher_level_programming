b4ff78575cfddb87207c7afe246e0142a8c5e546
NdirituG <denisndiritug@gmail.com>
2022-09-03 12:46:31 +0300

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
-
-    total = 0
-
-    for i in range(1, len(sys.argv)):
-        total += int(sys.argv[i])
-
-    print(total)
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
+    print(result)
